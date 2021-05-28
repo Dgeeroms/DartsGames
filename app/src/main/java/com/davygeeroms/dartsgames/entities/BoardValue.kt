efe9ca5177dbfbef2291351aeb133fa344cc2911
Davy Geeroms <davy.geeroms@student.hogent.be>
2021-05-28 11:01:24 +0200

    Added BoardValue - corresponding with the fields on the dartsboard

---
+++ app/src/main/java/com/davygeeroms/dartsgames/entities/BoardValue.kt
@@ -0,0 +1,5 @@
+package com.davygeeroms.dartsgames.entities
+
+class BoardValue(val value: Number, val modifier: Number, val description: String) {
+
+}
---
+++ app/src/main/java/com/davygeeroms/dartsgames/factories/BoardValueFactory.kt
@@ -0,0 +1,24 @@
+package com.davygeeroms.dartsgames.factories
+
+import com.davygeeroms.dartsgames.entities.BoardValue
+import com.davygeeroms.dartsgames.enums.BoardValues
+
+class BoardValueFactory {
+
+    fun getBoardValue(bv:BoardValues): BoardValue{
+
+        val modifierStr = bv.name.substring(1)
+        var modifierInt = 1
+        when(modifierStr){
+            "S" -> modifierInt = 1
+            "D" -> modifierInt = 2
+            "T" -> modifierInt = 3
+        }
+
+        return BoardValue(
+            bv.name.substring(1).toInt(),
+            modifierInt,
+            bv.valuesStr
+        )
+    }
+}
